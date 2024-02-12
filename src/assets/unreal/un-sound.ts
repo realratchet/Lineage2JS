288129823232ee8da94dec3356563f196440d035
Ratchet <audryzas@hotmail.com>
2024-02-12 21:54:03 +0200

    audio extract

--- src/assets/unreal/un-sound.ts
+++ src/assets/unreal/un-sound.ts
@@ -1,7 +1,30 @@
-import UObject from "@l2js/core";
+import UObject, { BufferValue } from "@l2js/core";
+import { FPrimitiveArrayLazy } from "@l2js/core/src/unreal/un-array";
 
 abstract class USound extends UObject {
-    public readonly careUnread: boolean = false;
+    protected fileType: string;
+    protected likelihood: number;
+    protected data = new FPrimitiveArrayLazy(BufferValue.uint8);
+
+    public doLoad(pkg: C.APackage, exp: C.UExport) {
+        super.doLoad(pkg, exp);
+
+        const compat = new BufferValue(BufferValue.compat32);
+        const nameIndex = pkg.read(compat).value;
+        
+        this.fileType = pkg.nameTable[nameIndex].name;
+        this.data.load(pkg);
+
+        this.readHead = pkg.tell();
+
+        const wav = this.data.getTypedArray().slice();
+        const blob = new Blob([wav.buffer], { type: "application/octet-stream" });
+        const f = URL.createObjectURL(blob);
+
+        console.log(f);
+
+        debugger;
+    }
 }
 
 export default USound;
