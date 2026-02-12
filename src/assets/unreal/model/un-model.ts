b92a74ef96ad5c781d703d1dc26386dfd985e1b4
Ratchet <audryzas@hotmail.com>
2026-02-12 07:54:09 +0200

    some cleanup, doesn't affect improperly

--- src/assets/unreal/model/un-model.ts
+++ src/assets/unreal/model/un-model.ts
@@ -531,12 +531,12 @@ abstract class UModel extends UPrimitive {
                 isUnlit: !!(sectionData.polyFlags & PF_Unlit),
                 sectionName: sectionKey,
                 // Sky level defaults or surface overrides
-                depthWrite: this.isSky ? false : undefined,
-                depthTest: this.isSky ? false : undefined,
-                fog: this.isSky ? true : undefined,
-                side: (this.isSky || (sectionData.polyFlags & PolyFlags_T.PF_TwoSided)) ? 2 : undefined, // 2 = DoubleSide
-                blendingMode: this.isSky ? (!(sectionData.polyFlags & PolyFlags_T.PF_TwoSided) ? "brighten" : "normal") :
-                    ((sectionData.polyFlags & PolyFlags_T.PF_Additive) ? "brighten" : undefined),
+                // depthWrite: this.isSky ? false : undefined,
+                // depthTest: this.isSky ? false : undefined,
+                // fog: this.isSky ? true : undefined,
+                // side: (this.isSky || (sectionData.polyFlags & PolyFlags_T.PF_TwoSided)) ? 2 : undefined, // 2 = DoubleSide
+                // blendingMode: this.isSky ? (!(sectionData.polyFlags & PolyFlags_T.PF_TwoSided) ? "brighten" : "normal") :
+                //     ((sectionData.polyFlags & PolyFlags_T.PF_Additive) ? "brighten" : undefined),
             };
 
             const sectionIndex = library.bspSections.length;
