f3db215b0d1e9214a8a4f09a7c7ee6e164cb82ae
Ratchet <audryzas@hotmail.com>
2026-08-20 10:56:39 +0300

    smol

--- src/assets/unreal/datafile/schema/hairaccessorylocgrp.schema.ts
+++ src/assets/unreal/datafile/schema/hairaccessorylocgrp.schema.ts
@@ -4,7 +4,7 @@ const SCHEMA_HAIRACCESSORYLOCGRP_DAT: ISchemaValue[] = [
     { type: "utf16", name: "name" }
 ];
 
-for (const suffix of ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]) {
+for (let i = 0x1, suffix = i.toString(16); i < 0xF; i++) {
     SCHEMA_HAIRACCESSORYLOCGRP_DAT.push({ type: new SizedContainerType("float", 3), name: `floats_${suffix}` });
     SCHEMA_HAIRACCESSORYLOCGRP_DAT.push({ type: new SizedContainerType("int32", 3), name: `ints_${suffix}` });
 }
